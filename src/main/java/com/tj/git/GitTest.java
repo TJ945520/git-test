4189aa9854c1556aacc930bed20b63742bc773a4
tanjie <tanjie@tj.com>
2023-05-16 16:12:32 +0800

    first commit

---
+++ src/main/java/com/tj/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.tj.git;
+
+/**
+ * @author TANJIE
+ * @date 2023/5/16 16:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
